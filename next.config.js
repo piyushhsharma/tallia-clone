b3dcb0dbb69e30c100dc731171f9c1caedf511a6
piyushhsharma <138559844+piyushhsharma@users.noreply.github.com>
2025-09-07 13:16:39 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,46 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  allowedDevOrigins: ["*.preview.same-app.com"],
-  images: {
-    unoptimized: true,
-    domains: [
-      "source.unsplash.com",
-      "images.unsplash.com",
-      "ext.same-assets.com",
-      "ugc.same-assets.com",
-    ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ext.same-assets.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ugc.same-assets.com",
-        pathname: "/**",
-      },
-    ],
-  },
-};
-
-module.exports = nextConfig;
 const isGitHub = process.env.GITHUB_ACTIONS === 'true';
 const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 👇 ensures Next.js exports static files
+  // ensures Next.js exports static files
   output: 'export',
   basePath: isGitHub ? `/${repo}` : '',
   assetPrefix: isGitHub ? `/${repo}/` : '',
